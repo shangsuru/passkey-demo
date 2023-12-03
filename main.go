e17115a0fc1c3d6520d0535d664e6bdd625220c9
shangsuru <45686061+shangsuru@users.noreply.github.com>
2023-12-03 17:46:11 +0900

    Begin registration

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-contrib/sessions"
+	gormsessions "github.com/gin-contrib/sessions/gorm"
+	"github.com/gin-gonic/gin"
+	"github.com/go-webauthn/webauthn/webauthn"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var webAuthn *webauthn.WebAuthn
+
+var sessionStore gormsessions.Store
+var userStore *UserDB
+
+func BeginRegistration(c *gin.Context) {
+	username := c.Param("username")
+
+	// get user
+	user, err := userStore.GetUser(username)
+	// user doesn't exist, create new user
+	if err != nil {
+		displayName := strings.Split(username, "@")[0]
+		user = NewUser(username, displayName)
+		userStore.PutUser(user)
+	}
+
+	// generate PublicKeyCredentialCreationOptions, session data
+	options, sessionData, err := webAuthn.BeginRegistration(user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// store session data as marshaled JSON
+	session := sessions.Default(c)
+	bytes, err := json.Marshal(sessionData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+	session.Set("registration", bytes)
+	err = session.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, options)
+}
+
+func main() {
+	var err error
+	webAuthn, err = webauthn.New(&webauthn.Config{
+		RPDisplayName: "PassKey Demo",
+		RPID:          "localhost",
+		RPOrigins:     []string{"localhost"},
+	})
+
+	if err != nil {
+		log.Fatal("failed to create WebAuthn from config:", err)
+	}
+
+	// Database
+	db, err := gorm.Open(sqlite.Open("db/test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Users
+	// TODO: use database
+	userStore = DB()
+
+	// Session
+	sessionStore = gormsessions.NewStore(db, true, []byte("secret"))
+
+	r := gin.Default()
+	r.Use(sessions.Sessions("mySession", sessionStore))
+
+	// Routes
+	r.LoadHTMLGlob("views/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	r.GET("/register/begin/:username", BeginRegistration)
+
+	_ = r.Run() // listen and serve on 0.0.0.0:8080
+}
