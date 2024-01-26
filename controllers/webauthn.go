6f3df5596c42895ef227db2da5fc30658ac5c180
shangsuru <45686061+shangsuru@users.noreply.github.com>
2024-01-26 12:47:50 +0900

    Refactor login and registration functions into webauthn controller

---
+++ controllers/webauthn.go
@@ -0,0 +1,171 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-webauthn/webauthn/webauthn"
+)
+
+type WebAuthnController interface {
+	BeginRegistration(c *gin.Context)
+	FinishRegistration(c *gin.Context)
+	BeginLogin(c *gin.Context)
+	FinishLogin(c *gin.Context)
+}
+
+type webAuthnController struct {
+	userStore *UserDB
+	webAuthn *webauthn.WebAuthn
+}
+
+func NewWebAuthnController() WebAuthnController {
+	webAuthn, err := webauthn.New(&webauthn.Config{
+		RPDisplayName: "PassKey Demo",
+		RPID:          "localhost",
+		RPOrigins:     []string{"http://localhost:8080"},
+	})
+
+	if err != nil {
+		log.Fatal("failed to create WebAuthn from config:", err)
+	}
+	return webAuthnController {
+		userStore: DB(),
+		webAuthn: webAuthn,
+	}
+}
+
+func (wc webAuthnController) BeginRegistration(c *gin.Context) {
+	username := c.Param("username")
+
+	// get user
+	user, err := wc.userStore.GetUser(username)
+	// user doesn't exist, create new user
+	if err != nil {
+		displayName := strings.Split(username, "@")[0]
+		user = NewUser(username, displayName)
+		wc.userStore.PutUser(user)
+	}
+
+	// generate PublicKeyCredentialCreationOptions, session data
+	options, sessionData, err := wc.webAuthn.BeginRegistration(user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := StoreSessionData(c, username, sessionData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, options)
+}
+
+func (wc webAuthnController) FinishRegistration(c *gin.Context) {
+	username := c.Param("username")
+	// get user
+	user, err := wc.userStore.GetUser(username)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	sessionData, err := LoadSessionData(c, username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	credential, err := wc.webAuthn.FinishRegistration(user, *sessionData, c.Request)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user.AddCredential(*credential)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Registration Success",
+	})
+}
+
+func (wc webAuthnController) BeginLogin(c *gin.Context) {
+	username := c.Param("username")
+	user, err := wc.userStore.GetUser(username)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// generate PublicKeyCredentialRequestOptions, session data
+	options, sessionData, err := wc.webAuthn.BeginLogin(user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := StoreSessionData(c, username, sessionData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, options)
+}
+
+func (wc webAuthnController) FinishLogin(c *gin.Context) {
+	username := c.Param("username")
+	user, err := wc.userStore.GetUser(username)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	sessionData, err := LoadSessionData(c, username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	// in an actual implementation we should perform additional
+	// checks on the returned 'credential'
+	_, err = wc.webAuthn.FinishLogin(user, *sessionData, c.Request)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// handle successful login
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login Success",
+	})
+}
--- main.go
+++ main.go
@@ -1,180 +1,26 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"strings"
+
+	"passkeys/controllers"
 
 	"github.com/gin-contrib/sessions"
 	gormsessions "github.com/gin-contrib/sessions/gorm"
 	"github.com/gin-gonic/gin"
-	"github.com/go-webauthn/webauthn/webauthn"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var webAuthn *webauthn.WebAuthn
-
-var sessionStore gormsessions.Store
-var userStore *UserDB
-
-func BeginRegistration(c *gin.Context) {
-	username := c.Param("username")
-
-	// get user
-	user, err := userStore.GetUser(username)
-	// user doesn't exist, create new user
-	if err != nil {
-		displayName := strings.Split(username, "@")[0]
-		user = NewUser(username, displayName)
-		userStore.PutUser(user)
-	}
-
-	// generate PublicKeyCredentialCreationOptions, session data
-	options, sessionData, err := webAuthn.BeginRegistration(user)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	if err := StoreSessionData(c, username, sessionData); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, options)
-}
-
-func FinishRegistration(c *gin.Context) {
-	username := c.Param("username")
-	// get user
-	user, err := userStore.GetUser(username)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	sessionData, err := LoadSessionData(c, username)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-	}
-
-	credential, err := webAuthn.FinishRegistration(user, *sessionData, c.Request)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	user.AddCredential(*credential)
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Registration Success",
-	})
-}
-
-func BeginLogin(c *gin.Context) {
-	username := c.Param("username")
-	user, err := userStore.GetUser(username)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// generate PublicKeyCredentialRequestOptions, session data
-	options, sessionData, err := webAuthn.BeginLogin(user)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	if err := StoreSessionData(c, username, sessionData); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, options)
-}
-
-func FinishLogin(c *gin.Context) {
-	username := c.Param("username")
-	user, err := userStore.GetUser(username)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	sessionData, err := LoadSessionData(c, username)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-	}
-
-	// in an actual implementation we should perform additional
-	// checks on the returned 'credential'
-	_, err = webAuthn.FinishLogin(user, *sessionData, c.Request)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	// handle successful login
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Login Success",
-	})
-}
-
 func main() {
-	var err error
-	webAuthn, err = webauthn.New(&webauthn.Config{
-		RPDisplayName: "PassKey Demo",
-		RPID:          "localhost",
-		RPOrigins:     []string{"http://localhost:8080"},
-	})
-
-	if err != nil {
-		log.Fatal("failed to create WebAuthn from config:", err)
-	}
-
-	// Database
+	// Database Setup
 	db, err := gorm.Open(sqlite.Open("db/test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	// Users
-	// TODO: use database
-	userStore = DB()
-
 	// Session
-	sessionStore = gormsessions.NewStore(db, true, []byte("secret"))
+	sessionStore := gormsessions.NewStore(db, true, []byte("secret"))
 
 	r := gin.Default()
 	r.Use(sessions.Sessions("mySession", sessionStore))
@@ -189,10 +35,11 @@ func main() {
 		c.HTML(http.StatusOK, "home.html", gin.H{})
 	})
 
-	r.GET("/register/begin/:username", BeginRegistration)
-	r.POST("/register/finish/:username", FinishRegistration)
-	r.GET("/login/begin/:username", BeginLogin)
-	r.POST("/login/finish/:username", FinishLogin)
+	webAuthnController := controllers.NewWebAuthnController()
+	r.GET("/register/begin/:username", webAuthnController.BeginRegistration)
+	r.POST("/register/finish/:username", webAuthnController.FinishRegistration)
+	r.GET("/login/begin/:username", webAuthnController.BeginLogin)
+	r.POST("/login/finish/:username", webAuthnController.FinishLogin)
 
-	_ = r.Run() // listen and serve on 0.0.0.0:8080
+	_ = r.Run()
 }
