f14ece5a0548d2ce17af1e232e1e501a9deffb33
shangsuru <45686061+shangsuru@users.noreply.github.com>
2024-06-21 20:30:26 +0900

    Add debugging config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+docker compose up -d 
+
 # Install frontend dependencies
 cd client && yarn install
 
 # Setup the database
-docker compose up -d 
 cd ../server && go run ./db/migration db init && go run ./db/migration db migrate
+
 docker compose down
